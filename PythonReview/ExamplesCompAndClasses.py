e35751a414d3b9c45d5d48057990dd304ba259e3
Olmo Zavala <olmozavala@gmail.com>
2023-01-20 16:57:25 -0500

    Refactoring

--- PythonReview/ExamplesCompAndClasses.py
+++ PythonReview/ExamplesCompAndClasses.py
@@ -1,16 +1,14 @@
-
-## ------ Basic syntact
-# print([f'My number is {x*2+1}' for x in range(10)])
-# print((f'My number is {x*2+1}' for x in range(10)))
-# x = {'a':1, 'b':2}
-# print({f'a{x}':x for x in range(10)})
+## ------ Comprehensions ---
+# Basic syntacs 
+print([f'My number is {x*2+1}' for x in range(10)])
+# Nested  Comp
 print([y*2 for y in [x for x in range(5)]])
 
-## ------
+## Filtering comp
 print([f'My number is {x*2+1}' for x in range(10) if x > 4])
 
-##
+## Conditional selection 
 print(['a' if x > 4 else 'b' for x in range(10) ])
 
-##
+## -------- Exceptions 
 
