c2ea8be2cf1aa8cc09b21f1b872d359e1bfe390b
Olmuz <olmozavala@gmail.com>
2023-02-15 00:03:20 -0500

    Filters examples

---
+++ FileFormats/Itk/Filters.py
@@ -0,0 +1,44 @@
+import imageio.v3 as iio
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy import signal
+
+## ------- Reading Image
+# Read images
+input_file = "./TestData/FiltersExampleLarge.png"
+img = iio.imread(input_file)[:,:,0]
+
+fig, ax = plt.subplots(1,1)
+ax.imshow(img, cmap='Greys_r')
+plt.show()
+
+## ---------- Mean filter ---------
+# mean_filter = np.ones((3,3)) * 1/9
+mean_filter = np.ones((5,5)) * 1/25
+img_mean= signal.convolve2d(img, mean_filter, boundary='symm', mode='same')
+
+fig, axs = plt.subplots(1,2, figsize=(10,5))
+im1 = axs[0].imshow(img, cmap='Greys_r', vmin=0, vmax=250)
+im2 = axs[1].imshow(img_mean, cmap='Greys_r', vmin=0, vmax=250)
+plt.colorbar(im1, ax=axs[0])
+plt.colorbar(im2, ax=axs[1])
+plt.tight_layout()
+plt.show()
+
+## --------- Edge detector --------
+sobelx = [[-1, 1],[-1, 1]]
+sobely = [[-1, -1],[1, 1]]
+# mean_filter = np.ones((5,5)) * 1/2
+img_x= signal.convolve2d(img_mean, sobelx, boundary='symm', mode='same')
+img_y= signal.convolve2d(img_mean, sobely, boundary='symm', mode='same')
+img_sobel = img_x+img_y
+
+fig, axs = plt.subplots(1,2, figsize=(10,5))
+im1 = axs[0].imshow(img, cmap='Greys_r', vmin=0, vmax=250)
+im2 = axs[1].imshow(img_sobel, cmap='Greys_r', vmin=0, vmax=250)
+plt.colorbar(im1, ax=axs[0])
+plt.colorbar(im2, ax=axs[1])
+plt.tight_layout()
+plt.show()
+##
+
---
+++ FileFormats/Itk/ITK_Examples.py
@@ -0,0 +1,46 @@
+import pytest
+import itk
+import matplotlib.pyplot as plt
+import numpy as np
+# API C++: https://itk.org/Doxygen/html/namespaceitk.html
+# Python https://itkpythonpackage.readthedocs.io/en/master/
+
+##
+# input_file = "./TestData/Breast_MRI.nii"
+# input_file = "./TestData/10647_1000663_cor.nii"
+input_file = "./TestData/Reg_2.nii"
+
+## ------- Reading Image
+# Read images
+img = itk.imread(input_file)
+# -- Read metadata as a dictionary
+metadata_dict = itk.dict_from_image(img)  # Fewer attributes, better names ('origin','spacing',etc.)
+metadata_dict = dict(img)  # All attributes, raw names (dim, pixdim, qoffset,...)
+# -- Access data as np arrays
+imge_np = itk.array_view_from_image(img)
+dims = imge_np.shape
+print(dims)
+
+## Showing planes
+fig, axs = plt.subplots(1, 3, figsize=(20,5))
+axs[0].imshow(imge_np[int(dims[0]/2), :, :])  # Trans
+axs[1].imshow(imge_np[:, int(dims[1]/2), :])  # Coronal
+axs[2].imshow(imge_np[:, :, int(dims[2]/3)])  # Sag
+plt.show()
+
+# ----------------- Filters ---------
+## Median filter
+smoothed = itk.mean_image_filter(img, radius=1)
+mean_filter = itk.MeanImageFilter.New(img)
+mean_filter.SetRadius(1)
+mean_filter.Update()
+smoothed2 = mean_filter.GetOutput()
+
+fig, axs = plt.subplots(1, 3, figsize=(22,5))
+axs[0].imshow(img[int(dims[0]/2), :, :])  # Trans
+axs[1].imshow(smoothed[int(dims[0]/2), :, :])  # Trans
+axs[2].imshow(smoothed2[int(dims[0]/2), :, :])  # Trans
+plt.tight_layout()
+plt.show()
+##
+
