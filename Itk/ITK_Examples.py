dc6f9245fca3817e5065243b4327341744f8fd80
Olmuz <olmozavala@gmail.com>
2023-02-16 23:09:49 -0500

    Resampling examples

--- Itk/ITK_Examples.py
+++ Itk/ITK_Examples.py
@@ -4,20 +4,22 @@ import matplotlib.pyplot as plt
 import numpy as np
 # API C++: https://itk.org/Doxygen/html/namespaceitk.html
 # Python https://itkpythonpackage.readthedocs.io/en/master/
+import matplotlib as mpl
+mpl.rc('image', cmap='gray')
 
 ##
 input_file = "./Data/MRI/t2_prostate_coronal.nii"
 
 ## ------- Reading Images data ------
 # Read images
-img = itk.imread(input_file)
+image_original = itk.imread(input_file)
 # -- Read metadata as a dictionary
-metadata_dict = itk.dict_from_image(img)  # Fewer attributes, better names ('origin','spacing',etc.)
+metadata_dict = itk.dict_from_image(image_original)  # Fewer attributes, better names ('origin','spacing',etc.)
 print(metadata_dict)
-metadata_dict = dict(img)  # All attributes, raw names (dim, pixdim, qoffset,...)
+metadata_dict = dict(image_original)  # All attributes, raw names (dim, pixdim, qoffset,...)
 print(metadata_dict)
 # -- Access data as np arrays
-imge_np = itk.array_view_from_image(img)
+imge_np = itk.array_view_from_image(image_original)
 dims = imge_np.shape
 print(f"Dimensions of the image are: {dims}")
 
@@ -35,14 +37,14 @@ plt.show()
 # ----------------- Filters ---------
 ## Median filter
 print("Computing Median filter...")
-smoothed = itk.mean_image_filter(img, radius=1)
-mean_filter = itk.MeanImageFilter.New(img)
+smoothed = itk.mean_image_filter(image_original, radius=3)
+mean_filter = itk.MeanImageFilter.New(image_original)
 mean_filter.SetRadius(4)
 mean_filter.Update()
 smoothed2 = mean_filter.GetOutput()
 
 fig, axs = plt.subplots(1, 3, figsize=(12,5))
-axs[0].imshow(img[int(dims[0]/2), :, :])  # Trans
+axs[0].imshow(image_original[int(dims[0]/2), :, :])  # Trans
 axs[0].set_title('Original')
 axs[1].imshow(smoothed[int(dims[0]/2), :, :])  # Trans
 axs[1].set_title('Smoothed (mean filter)')
@@ -53,16 +55,57 @@ plt.show()
 
 ## Sobel filter
 print("Computing Sobel filter...")
-input_image = itk.imread(input_file, pixel_type=itk.F)
-smoothed = itk.sobel_edge_detection_image_filter(input_image)
-##
+smoothed = itk.sobel_edge_detection_image_filter(image_original)
+
 fig, axs = plt.subplots(1, 2, figsize=(12,5))
-axs[0].imshow(img[int(dims[0]/2), :, :])  # Trans
+axs[0].imshow(image_original[int(dims[0]/2), :, :])  # Trans
 axs[0].set_title('Original')
 axs[1].imshow(smoothed[int(dims[0]/2), :, :], cmap='Greys_r')  # Trans
 axs[1].set_title('Edge detction (sobel filter)')
 plt.tight_layout()
 plt.show()
 
+
+## Resample
+print("Original spacing: ", image_original.GetSpacing())
+print("Original origin: ", image_original.GetOrigin())
+print("Resampling...")
+new_spacing = [0.3, 0.3, 3.0]  # spacing in mm
+
+interpolator = itk.LinearInterpolateImageFunction.New(image_original)
+resampler = itk.ResampleImageFilter.New(image_original)
+
+# Set the resampling parameters
+resampler.SetOutputSpacing(new_spacing)
+print(itk.size(image_original))
+resampler.SetSize(itk.size(image_original))
+# resampler.SetSize((720,720,25))
+resampler.SetOutputDirection(image_original.GetDirection())
+resampler.SetOutputOrigin(image_original.GetOrigin())
+# resampler.SetOutputOrigin((-99, 6.80609, 177.443))
+
+# Set the interpolation method
+resampler.SetInterpolator(interpolator)
+
+# Execute the resampling
+resampler.Update()
+
+# Get the resampled output image
+resampled = resampler.GetOutput()
+
+print("Done!")
+
+print(f"Dimensions of the original image are: {image_original.shape}")
+print(f"Dimensions of the image are: {resampled.shape}")
+print(f"Spacing of the original image are: {image_original.GetSpacing()}")
+print(f"Spacing of the image are: {resampled.GetSpacing()}")
+
+fig, axs = plt.subplots(1, 2, figsize=(12,5))
+axs[0].imshow(image_original[int(dims[0]/2), :, :])  # Trans
+axs[0].set_title('Original')
+axs[1].imshow(resampled[int(dims[0]/2), :, :], cmap='Greys_r')  # Trans
+axs[1].set_title('Resampled image')
+plt.tight_layout()
+plt.show()
 ##
 
--- Preprocessing/Filters.py
+++ Preprocessing/Filters.py
@@ -1,6 +1,7 @@
 import imageio.v3 as iio
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.ndimage
 from scipy import signal
 
 ## ------- Reading Image
@@ -40,5 +41,80 @@ plt.colorbar(im2, ax=axs[1])
 plt.tight_layout()
 plt.show()
 print("Done")
+
+## ------------- Dilation -------------
+image = [[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+image = np.array(image)
+
+kernel = [[0, 1, 0],
+          [1, 1, 1],
+          [0, 1, 0]]
+
+kernel = np.array(kernel)
+
+def dilation(image, kernel):
+    m, n = image.shape
+    a, b = kernel.shape
+    output = np.zeros((m, n))
+    for i in range(m):
+        for j in range(n):
+            if image[i, j] == 1:
+                for k in range(a):
+                    for l in range(b):
+                        if kernel[k, l] == 1:
+                            p = i + (k - 1)
+                            q = j + (l - 1)
+                            if p >= 0 and p < m and q >= 0 and q < n:
+                                output[p, q] = 1
+    return output
+
+def erosion(image, kernel):
+    m, n = image.shape
+    a, b = kernel.shape
+    output = np.zeros((m, n))
+    for i in range(m):
+        for j in range(n):
+            if image[i, j] == 1:
+                flag = 1
+                for k in range(a):
+                    for l in range(b):
+                        if kernel[k, l] == 1:
+                            p = i + (k - 1)
+                            q = j + (l - 1)
+                            if p >= 0 and p < m and q >= 0 and q < n:
+                                if image[p, q] == 0:
+                                    flag = 0
+                if flag == 1:
+                    output[i, j] = 1
+    return output
+
+dilated = dilation(image, kernel)
+dilated_scipy = scipy.ndimage.binary_dilation(image, kernel)
+eroded = erosion(image, kernel)
+
+fig, axs = plt.subplots(2,2, figsize=(10,5))
+axs[0][0].imshow(image, cmap='Greys_r')
+axs[0][0].set_title(f"Original {image.shape}")
+axs[0][1].imshow(kernel)
+axs[0][1].set_title(f"Structure {kernel.shape}")
+# axs[1][0].imshow(dilated, cmap='Greys_r')
+axs[1][0].imshow(dilated_scipy, cmap='Greys_r')
+axs[1][0].set_title("Dilated")
+axs[1][1].imshow(eroded, cmap='Greys_r')
+axs[1][1].set_title("Eroded")
+plt.tight_layout()
+plt.show()
+print("Done")
+
 ##
 
